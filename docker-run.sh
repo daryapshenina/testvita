c4527396e397a30adc578e67dd90a28f2c27e024
psheninada <psheninada@altarix.ru>
2018-12-21 18:35:46 +0400

    изменены данные подключения к бд под локальную разработку

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -d -p 8081:80 -p 8083:8083 -p 27017:27017 -p 222:22 -p 54322:5432 --name vitaweb -v $(pwd):/var/www vitaweb
+docker run -d -p 8085:80 -p 8083:8083  -p 223:22 -p 54322:5432 --name vitaweb -v $(pwd):/var/www vitaweb
 
